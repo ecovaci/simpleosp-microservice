25cef0d3c1ae21ddd269319d6afec8c859cb7582
ecovaci <eugen.covaci.q@gmail.com>
2019-01-16 20:08:18 +0200

    4th attempt.

--- src/main/java/org/hello/controller/HelloController.java
+++ src/main/java/org/hello/controller/HelloController.java
@@ -1,15 +1,15 @@
 package org.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello!";
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hi " + name + " !";
     }
-
 }
