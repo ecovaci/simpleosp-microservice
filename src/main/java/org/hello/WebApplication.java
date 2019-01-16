9105dccc079a61681873b42cbfc1674c821f3de4
ecovaci <eugen.covaci.q@gmail.com>
2019-01-16 18:05:42 +0200

    First attempt.

---
+++ src/main/java/org/hello/WebApplication.java
@@ -0,0 +1,8 @@
+package org.hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+}
